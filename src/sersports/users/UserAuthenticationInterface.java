15a90e97e6fcc2fd9608b794c4e46ef128ed3e64
Graydon Svendson <graydonsvendson@yahoo.com>
2015-02-14 10:43:56 -0700

    Resetting Master Branch to a clean slate
    This commit, deletes all of the source files, and formats the folders
    to the correct way.

--- src/sersports/users/UserAuthenticationInterface.java
+++
@@ -1,9 +0,0 @@
-package sersports.users;
-
-public interface UserAuthenticationInterface 
-{
-	// Returns true if the user is authenticate
-	Boolean authenticateUser(String userName, String password);
-	// Return true if a user is added
-	Boolean addUser(String userName, String password);
-}
