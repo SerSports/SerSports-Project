e71ed16f009da429210733a3b22cb00912597a89
licatak <klicata@asu.edu>
2015-02-23 20:33:31 -0700

    added MLBPlayerSearch code

---
+++ src/logic/MLBPlayerSearch.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+
+public class MLBPlayerSearch {
+
+	//constructor
+	public MLBPlayerSearch(){
+	}
+	
+	//constructor with 4 parameters
+	public MLBPlayerSearch(String fName, String lName, String position, String team) throws IOException {
+		
+		//if all parameters are null
+			//either list all available players to look through
+			//or list nothing
+			
+		//else
+			//search first name, last name, position, and/or team
+			//add the list of possible solutions into a temporary db?
+			
+		
+	}
+}
