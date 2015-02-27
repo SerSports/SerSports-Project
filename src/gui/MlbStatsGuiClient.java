293202aa06fa80ef83218207002dfc4c90ea7b04
aamannon <aamannon@asu.edu>
2015-02-27 13:58:12 -0700

    small update client

--- src/gui/MlbStatsGuiClient.java
+++ src/gui/MlbStatsGuiClient.java
@@ -1,5 +1,29 @@
 package gui;
 
-public class MlbStatsGuiClient {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+
+public class MlbStatsGuiClient extends MlbStatsGui implements ActionListener, ItemListener {
+
+	public MlbStatsGuiClient(String fName, String lName, String team) {
+		//btnComapreToPlayer.addActionListener(this);
+		//btnSeePlayerStats
+		//SubmitPlayerSearchButton
+	}
+	
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
