325aba498aab837acd287bdafba764d15a5f134d
aamannon <aamannon@asu.edu>
2015-02-27 14:24:23 -0700

    variable inits
    moved the jbuttons to be declared in the class, left initialization
    where it was

--- src/gui/MlbStatsGui.java
+++ src/gui/MlbStatsGui.java
@@ -21,6 +21,9 @@ public class MlbStatsGui extends JPanel {
 	private JTable table;
 	private JTextField txtLastName;
 	private JTextField txtTeam;
+	protected JButton btnSeePlayerStats;
+	protected JButton btnCompareToPlayer;
+	protected JButton SubmitPlayerSearchButton;
 
 
 	/**
@@ -31,48 +34,48 @@ public class MlbStatsGui extends JPanel {
 		setLayout(null);
 		getDBconnection();
 		getResultsSet();
-		
+
 		/*JLabel logo = new JLabel("New label");
 		Image img = new ImageIcon(this.getClass().getResource("/Logo1.png")).getImage();
 		logo.setIcon(new ImageIcon(img));
 		logo.setBounds(589, 6, 100, 100);
 		add(logo);*/
-		
-		JButton SubmitPlayerSearchButton = new JButton("Submit");
+
+		SubmitPlayerSearchButton = new JButton("Submit");
 		SubmitPlayerSearchButton.setBounds(581, 234, 117, 29);
 		add(SubmitPlayerSearchButton);
 		SubmitPlayerSearchButton.setActionCommand("SubmitPlayerSearchButton");
 		SubmitPlayerSearchButton.setBackground(new Color(255,237,203));
 	    //getContentPane().add(SubmitPlayerSearchButton);
-		
+
 		txtEnterNameteamPosition = new JTextField();
 		txtEnterNameteamPosition.setText("First Name");
 		txtEnterNameteamPosition.setBounds(549, 118, 180, 29);
 		add(txtEnterNameteamPosition);
 		txtEnterNameteamPosition.setColumns(10);
 
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(400, 288, 478, 246);
 		add(scrollPane);
-		
+
 		table = new JTable(new DefaultTableModel(null, new Object[]{"First Name", "Last Name", "Team"/* "Position" */}));
 		scrollPane.setViewportView(table);
-		
-		JButton btnSeePlayerStats = new JButton("See Player Stats");
+
+		btnSeePlayerStats = new JButton("See Player Stats");
 		btnSeePlayerStats.setBounds(421, 594, 200, 50);
 		add(btnSeePlayerStats);
-		
-		JButton btnComapreToPlayer = new JButton("Compare to Player");
-		btnComapreToPlayer.setBounds(656, 594, 200, 50);
-		add(btnComapreToPlayer);
-		
+
+		btnCompareToPlayer = new JButton("Compare to Player");
+		btnCompareToPlayer.setBounds(656, 594, 200, 50);
+		add(btnCompareToPlayer);
+
 		txtLastName = new JTextField();
 		txtLastName.setText("Last Name");
 		txtLastName.setColumns(10);
 		txtLastName.setBounds(549, 153, 180, 28);
 		add(txtLastName);
-		
+
 		txtTeam = new JTextField();
 		txtTeam.setText("Team");
 		txtTeam.setColumns(10);
@@ -84,12 +87,12 @@ public class MlbStatsGui extends JPanel {
 
 	private void getResultsSet() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 
 	private void getDBconnection() {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
