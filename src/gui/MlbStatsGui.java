4573188be2e0e49134de50199e6f2748748b4b9d
gtikhono <gtikhono@asu.edu>
2015-03-04 13:30:02 -0700

    edited button

--- src/gui/MlbStatsGui.java
+++ src/gui/MlbStatsGui.java
@@ -78,12 +78,12 @@ public class MlbStatsGui extends JPanel {
 		table = new JTable(new DefaultTableModel(null, new Object[]{"First Name", "Last Name", "Team"/* "Position" */}));
 		scrollPane.setViewportView(table);
 		
-		btnSeePlayerStats = new JButton("See Player Stats");
+		/*btnSeePlayerStats = new JButton("See Player Stats");
 		btnSeePlayerStats.setBounds(421, 594, 200, 50);
 		btnSeePlayerStats.setActionCommand("SeePlayerStats");
 		add(btnSeePlayerStats);
 		
-		/*(btnCompareToPlayer = new JButton("Compare to Player");
+		(btnCompareToPlayer = new JButton("Compare to Player");
 		btnCompareToPlayer.setBounds(656, 594, 200, 50);
 		btnCompareToPlayer.setActionCommand("CompareToPlayer");
 		add(btnCompareToPlayer);*/
