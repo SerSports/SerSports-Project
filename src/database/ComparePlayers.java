4f968f90fab0e98d6f37271d65333cc244dd0658
Colton <carose3@asu.edu>
2015-04-04 11:43:01 -0700

    Updates to ComparePlayers for stub

--- src/database/ComparePlayers.java
+++ src/database/ComparePlayers.java
@@ -4,13 +4,24 @@ import java.util.ArrayList;
 
 public class ComparePlayers {
 	
+	private float[] LocalScores;
+	private float[] MlbScores;
 	
-	public static MlbPlayer compareToPlayerList(LocalPlayer lp, ArrayList<MlbPlayer> mlbList) {
-		return mlbList.get(0);
+	public static ArrayList<MlbPlayer> compareToPlayerList(LocalPlayer lp, ArrayList<MlbPlayer> mlbList) {
+		return mlbList;
 	}
 	
 	public static float compareToPlayer(LocalPlayer lb, MlbPlayer mlbPlayer) {
 		return 50.0f;
+	}
+	
+	private int returnDifference(int firstNum, int secNum, int[] differenceNums) {
+		int startScore = 1000;
+		return 1;
+	}
+	
+	private void loadScores(LocalPlayer lb, MlbPlayer mlbPlayer) {
+		
 	}
 
 }
