7c320a8256137920263ea9abee0ca0cd5191d261
Colton <carose3@asu.edu>
2015-04-07 21:36:14 -0700

    Fleshing out ComparePlayers class

--- src/database/ComparePlayers.java
+++ src/database/ComparePlayers.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 
 public class ComparePlayers {
 	
-	private static float[] localScores;
-	private static float[] mlbScores;
+	private static float[] localScore;
+	private static float[] mlbScore;
 	private static float[] differenceNums;
 	
 	public static ArrayList<MlbPlayer> compareToPlayerList(LocalPlayer lp, ArrayList<MlbPlayer> mlbList) {
@@ -27,6 +27,15 @@ public class ComparePlayers {
 	}
 	
 	private static void loadScores(LocalPlayer lb, MlbPlayer mlbPlayer) {
+		// Load Local Player Stats
+		ArrayList<LocalPlayerBattingStatistics> lpStatsBat = LocalPlayerBattingStatistics.getStatisticsFromDatabase(lb.getLocalPlayerId());
+		LocalPlayerBattingStatistics lbStats = lpStatsBat.get(0);
+		float[] localScores = {lbStats.getHitting_games_play(), lbStats.getHitting_ab(), lbStats.getHitting_runs_total(), lbStats.getHitting_onbase_h(), 
+				lbStats.getHitting_onbase_d(), lbStats.getHitting_onbase_t(), lbStats.getHitting_onbase_hr(), lbStats.getHitting_rbi(),
+				lbStats.getHitting_outs_ktotal(), lbStats.getHitting_steal_stolen()};
+		
+		// Load Mlb Player Stats
+		float [] mlbScores = {};
 		System.out.println("Not implemented");
 	}
 
--- src/database/LocalPlayerBattingStatistics.java
+++ src/database/LocalPlayerBattingStatistics.java
@@ -164,7 +164,7 @@ public class LocalPlayerBattingStatistics {
 		ResultSet rs = Database.getResultSetFromSQL(sql);
 
 		if (rs != null) {
-			// Loop through the Result Set and Add Each MlbPlayer to the
+			// Loop through the Result Set and Add Each LocalPlayer to the
 			// ArrayList
 			try {
 				while (rs.next()) {
@@ -267,6 +267,11 @@ public class LocalPlayerBattingStatistics {
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
+	}
+	
+	public static void deleteLocalPlayerBattingStatistic(int localPlayerHittingStatisticID){
+		Database.executeSQL("DELETE FROM " + TABLE_NAME + " WHERE " + FIELD_ID + " = " 
+				+ localPlayerHittingStatisticID + ";");
 	}
 	
 	private static boolean notNumeric(String value){
