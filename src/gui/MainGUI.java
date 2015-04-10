0260e96aaf60f00fe417de47c8d5bd67881fb28a
gtikhono <gtikhono@asu.edu>
2015-04-10 16:50:46 -0700

    changed sign-in page a little

--- src/gui/MainGUI.java
+++ src/gui/MainGUI.java
@@ -10,6 +10,7 @@ package gui;
 import java.awt.CardLayout;
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -17,6 +18,7 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 
 import database.User;
+
 import java.awt.BorderLayout;
 
 /**
@@ -54,32 +56,40 @@ public class MainGUI{
     }
        
     public MainGUI(){    	
-    	mainFrame.add(scrollPane);
+    	mainFrame.getContentPane().add(scrollPane);
     	scrollPane.setBorder(BorderFactory.createEmptyBorder());
     	scrollPane.setViewportView(panelContainer);
     	login.setLayout(null);
     	txtUserName = new JTextField();
-    	txtUserName.setBounds(244, 5, 134, 28);
+    	txtUserName.setBounds(432, 199, 135, 28);
     	txtUserName.setText("ser_sports");
     	login.add(txtUserName);
     	txtUserName.setColumns(10);
-    	pwdPassword.setBounds(383, 5, 134, 28);
+    	pwdPassword.setBounds(432, 253, 135, 28);
     	pwdPassword.setColumns(10);
     	pwdPassword.setText("admin");
     	login.add(pwdPassword);	
     	
     	JButton btnSubmit_1 = new JButton("Submit");
-    	btnSubmit_1.setBounds(522, 5, 88, 29);
+    	btnSubmit_1.setBounds(456, 300, 88, 29);
     	login.add(btnSubmit_1);
     	
     	JButton btnCreateAccount_1 = new JButton("Create Account");
-    	btnCreateAccount_1.setBounds(615, 5, 140, 29);
+    	btnCreateAccount_1.setBounds(430, 341, 140, 29);
     	login.add(btnCreateAccount_1);
     	
     			//create and index the Panels
     			panelApplication = new ApplicationGUI();
     			panelContainer.setLayout(c1);
     			panelContainer.add(login, "1");
+    			
+    			JLabel lblbackgroundImage = new JLabel("");
+    			lblbackgroundImage.setBounds(-908, -627, 3456, 1446);
+    			login.add(lblbackgroundImage);
+    			Image img = new ImageIcon(this.getClass().getResource("/images/Backgroundimage.png")).getImage();
+    			lblbackgroundImage.setIcon(new ImageIcon(img));
+    			
+    			
     			panelContainer.add(panelApplication,"2");
     			panelContainer.add(createAccountGUI, "3");
     			c1.show(panelContainer, "1");
@@ -108,7 +118,7 @@ public class MainGUI{
     	mainFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     	mainFrame.pack();
     	mainFrame.setVisible(true);
-    	mainFrame.setSize(1024, 768);
+    	//mainFrame.setSize(1024, 768);
     	Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
     	mainFrame.setLocation(dim.width/2-mainFrame.getSize().width/2, dim.height/2-mainFrame.getSize().height/2);
     	
