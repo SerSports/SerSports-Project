8f1d62e520abc776448e6f995aabb116207e36bb
gtikhono <gtikhono@asu.edu>
2015-04-10 12:43:16 -0700

    Adding JLabels over textfields

--- src/gui/UserFieldingStats.java
+++ src/gui/UserFieldingStats.java
@@ -67,76 +67,76 @@ public class UserFieldingStats extends JPanel {
 		setLayout(null);
 		
 		JLabel lblYouAre = new JLabel("Enter a new game:");
-		lblYouAre.setForeground(Color.BLACK);
-		lblYouAre.setBounds(90, 124, 119, 16);
+		lblYouAre.setForeground(new Color(244, 229, 192));
+		lblYouAre.setBounds(31, 25, 119, 16);
 		add(lblYouAre);
 		
 		txtDate = new JTextField();
 		txtDate.setText("Date");
-		txtDate.setBounds(221, 118, 92, 28);
+		txtDate.setBounds(56, 96, 105, 22);
 		add(txtDate);
 		txtDate.setColumns(10);
 		
 		txtGP = new JTextField();
 		txtGP.setText("GP");
 		txtGP.setColumns(10);
-		txtGP.setBounds(325, 118, 77, 28);
+		txtGP.setBounds(56, 140, 105, 22);
 		add(txtGP);
 		
 		txtWins = new JTextField();
 		txtWins.setText("Wins");
 		txtWins.setColumns(10);
-		txtWins.setBounds(414, 118, 77, 28);
+		txtWins.setBounds(56, 184, 105, 22);
 		add(txtWins);
 
 		
 		txtLoss = new JTextField();
 		txtLoss.setText("Losses");
 		txtLoss.setColumns(10);
-		txtLoss.setBounds(511, 118, 77, 28);
+		txtLoss.setBounds(215, 96, 105, 22);
 		add(txtLoss);
 		
 		txtPo = new JTextField();
 		txtPo.setText("PO");
 		txtPo.setColumns(10);
-		txtPo.setBounds(620, 118, 77, 28);
+		txtPo.setBounds(215, 140, 105, 22);
 		add(txtPo);
 		
 		txtE = new JTextField();
 		txtE.setText("Err");
 		txtE.setColumns(10);
-		txtE.setBounds(709, 118, 77, 28);
+		txtE.setBounds(215, 184, 105, 22);
 		add(txtE);
 		
 		txtA = new JTextField();
 		txtA.setText("Assist");
 		txtA.setColumns(10);
-		txtA.setBounds(798, 118, 77, 28);
+		txtA.setBounds(352, 96, 105, 22);
 		add(txtA);
 		
 		txtFpct = new JTextField();
 		txtFpct.setText("F%");
 		txtFpct.setColumns(10);
-		txtFpct.setBounds(887, 118, 77, 28);
+		txtFpct.setBounds(352, 140, 105, 22);
 		add(txtFpct);
 		
 		
 		SubmitFieldingStats = new JButton("Submit");
-		SubmitFieldingStats.setBounds(580, 181, 117, 29);
+		SubmitFieldingStats.setBounds(524, 117, 117, 29);
 		add(SubmitFieldingStats);
 		SubmitFieldingStats.setActionCommand("SubmitFieldingStats");
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(293, 235, 691, 235);
+		scrollPane.setBounds(27, 243, 691, 235);
 		add(scrollPane);
 		
 		btnUpdateStatistic = new JButton("Update Statistic");
-		btnUpdateStatistic.setBounds(469, 504, 143, 28);
+		btnUpdateStatistic.setBounds(227, 504, 143, 28);
 		add(btnUpdateStatistic);
 		btnUpdateStatistic.setActionCommand("UpdateStatistic");
 		
 		btnDeleteStatistic = new JButton("Delete Statistic");
-		btnDeleteStatistic.setBounds(674, 504, 143, 28);
+		btnDeleteStatistic.setBounds(423, 504, 143, 28);
 		add(btnDeleteStatistic);
 		btnDeleteStatistic.setActionCommand("DeleteStatistic");
 
