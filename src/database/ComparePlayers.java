2c9f96b736e4f49724f815c7ff3ef082e276158a
Colton <carose3@asu.edu>
2015-04-11 11:05:19 -0700

    Added new get statements to LocalPlayerBattingStatistics and MlbPlayer for total bases and slugging

--- src/database/ComparePlayers.java
+++ src/database/ComparePlayers.java
@@ -28,17 +28,29 @@ public class ComparePlayers {
 	
 	private static void loadScores(LocalPlayer lb, MlbPlayer mlbPlayer) {
 		// Load Local Player Stats
+		
 		ArrayList<LocalPlayerBattingStatistics> lpStatsBat = LocalPlayerBattingStatistics.getStatisticsFromDatabase(lb.getLocalPlayerId());
+		
+		
 		LocalPlayerBattingStatistics lbStats = lpStatsBat.get(0);
+		// local player batting average
+		float battingAverageLocal = 0.00f;
+		if (lbStats.getHitting_ab() != 0) {
+			battingAverageLocal = lbStats.getHitting_onbase_h() / lbStats.getHitting_ab();
+		}
+		else {
+			battingAverageLocal = 0.00f;
+		}
+		
 		float[] localScores = {lbStats.getHitting_games_play(), lbStats.getHitting_ab(), lbStats.getHitting_runs_total(), lbStats.getHitting_onbase_h(), 
 				lbStats.getHitting_onbase_d(), lbStats.getHitting_onbase_t(), lbStats.getHitting_onbase_hr(), lbStats.getHitting_rbi(),
-				lbStats.getHitting_outs_ktotal(), lbStats.getHitting_steal_stolen()};
+				lbStats.getHitting_outs_ktotal(), lbStats.getHitting_steal_stolen(), battingAverageLocal};
 		localScore = localScores;
 		
 		// Load Mlb Player Stats
 		float [] mlbScores  = {mlbPlayer.getHitting_games_play(), mlbPlayer.getHitting_ab(), mlbPlayer.getHitting_runs_total(), mlbPlayer.getHitting_onbase_h(), 
 				mlbPlayer.getHitting_onbase_d(), mlbPlayer.getHitting_onbase_t(), mlbPlayer.getHitting_onbase_hr(), mlbPlayer.getHitting_rbi(),
-				mlbPlayer.getHitting_outs_ktotal(), mlbPlayer.getHitting_steal_stolen()};
+				mlbPlayer.getHitting_outs_ktotal(), mlbPlayer.getHitting_steal_stolen(), mlbPlayer.getBattingAverage()};
 		mlbScore = mlbScores;
 		System.out.println("Not implemented yet");
 	}
--- src/database/LocalPlayerBattingStatistics.java
+++ src/database/LocalPlayerBattingStatistics.java
@@ -61,6 +61,17 @@ public class LocalPlayerBattingStatistics {
 	public String getTeam_name() {
 		return team_name;
 	}
+	
+	public int getTotalBases() {
+		return hitting_onbase_h + (2 * hitting_onbase_d) + (3 * hitting_onbase_t) + (4 * hitting_onbase_hr);
+	}
+	
+	public float getSlugging() {
+		if (hitting_ab != 0) {
+			return getTotalBases() / hitting_ab;
+		}
+		return 0.00f;
+	}
 
 	public String getPosition() {
 		return position;
