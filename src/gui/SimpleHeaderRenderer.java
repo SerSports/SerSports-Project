c501f71794475a82bf69dde0b733fbc0b1ada338
gtikhono <gtikhono@asu.edu>
2015-04-11 14:06:43 -0700

    playing with table

--- src/gui/SimpleHeaderRenderer.java
+++ src/gui/SimpleHeaderRenderer.java
@@ -20,8 +20,10 @@ public class SimpleHeaderRenderer extends JLabel implements TableCellRenderer {
     public SimpleHeaderRenderer() {
         setFont(new Font("Montserrat", Font.BOLD, 14));
         setForeground(Color.BLACK);
-        setBackground(new Color(47, 52, 64));
-        setBorder(BorderFactory.createEtchedBorder());
+        //setBackground(new Color(47, 52, 64));
+        //setBorder(BorderFactory.createEtchedBorder());
+        setBorder(BorderFactory.createEmptyBorder());
+        
     }
      
     @Override
