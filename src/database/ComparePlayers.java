33a9fe938adb4baa6de440fe4bb1a6e691028389
Colton <carose3@asu.edu>
2015-04-11 11:57:21 -0700

    Almost completed ComparePlayers class, the only thing not working is returning a list of 10 MlbPlayers that are the best match to the local player.

--- src/database/ComparePlayers.java
+++ src/database/ComparePlayers.java
@@ -6,24 +6,38 @@ public class ComparePlayers {
 	
 	private static float[] localScore;
 	private static float[] mlbScore;
-	private static float[] differenceNums;
+	private static int[] differenceNums = {20, 75, 10, 15, 5, 4, 2, 10, 25, 150, 20};
+	private static float localBAverage;
+	private static float localSlug;
+	private static float mlbBAverage;
+	private static float mlbSlug;
 	
 	public static ArrayList<MlbPlayer> compareToPlayerList(LocalPlayer lp, ArrayList<MlbPlayer> mlbList) {
 		return mlbList;
 	}
 	
+	
 	public static float compareToPlayer(LocalPlayer lb, MlbPlayer mlbPlayer) {
 		loadScores(lb, mlbPlayer);
 		float startScore = 100.00f;
 		for(int i = 0; i < differenceNums.length; i++) {
-			startScore -= returnDifference(localScore[i], mlbScore[i]);
+			startScore -= returnDifference(localScore[i], mlbScore[i], i);
 		}
-		return 50.0f;
+		startScore -= returnDifference(localBAverage, mlbBAverage, .001f);
+		startScore -= returnDifference(localSlug, mlbSlug, .002f);
+		return startScore;
 	}
 	
-	private static int returnDifference(float firstNum, float secNum) {
-		
-		return 1;
+	private static float returnDifference(float firstNum, float secNum, int position) {
+		float difference = firstNum - secNum;
+		int remainder = (int)(difference % differenceNums[position]);
+		return .01f * remainder;
+	}
+	
+	private static float returnDifference(float firstNum, float secNum, float differenceNum) {
+		float difference = firstNum - secNum;
+		int remainder = (int)(difference % differenceNum);
+		return .01f * remainder;
 	}
 	
 	private static void loadScores(LocalPlayer lb, MlbPlayer mlbPlayer) {
@@ -32,26 +46,30 @@ public class ComparePlayers {
 		ArrayList<LocalPlayerBattingStatistics> lpStatsBat = LocalPlayerBattingStatistics.getStatisticsFromDatabase(lb.getLocalPlayerId());
 		
 		
-		LocalPlayerBattingStatistics lbStats = lpStatsBat.get(0);
+		LocalPlayerBattingStatistics lpStats = lpStatsBat.get(0);
 		// local player batting average
 		float battingAverageLocal = 0.00f;
-		if (lbStats.getHitting_ab() != 0) {
-			battingAverageLocal = lbStats.getHitting_onbase_h() / lbStats.getHitting_ab();
+		if (lpStats.getHitting_ab() != 0) {
+			battingAverageLocal = lpStats.getHitting_onbase_h() / lpStats.getHitting_ab();
 		}
 		else {
 			battingAverageLocal = 0.00f;
 		}
 		
-		float[] localScores = {lbStats.getHitting_games_play(), lbStats.getHitting_ab(), lbStats.getHitting_runs_total(), lbStats.getHitting_onbase_h(), 
-				lbStats.getHitting_onbase_d(), lbStats.getHitting_onbase_t(), lbStats.getHitting_onbase_hr(), lbStats.getHitting_rbi(),
-				lbStats.getHitting_outs_ktotal(), lbStats.getHitting_steal_stolen(), battingAverageLocal};
+		float[] localScores = {lpStats.getHitting_games_play(), lpStats.getHitting_ab(), lpStats.getHitting_runs_total(), lpStats.getHitting_onbase_h(), 
+				lpStats.getHitting_onbase_d(), lpStats.getHitting_onbase_t(), lpStats.getHitting_onbase_hr(), lpStats.getHitting_rbi(),
+				lpStats.getHitting_outs_ktotal(), lpStats.getHitting_steal_stolen()};
+		localBAverage = battingAverageLocal;
+		localSlug = lpStats.getSlugging();
 		localScore = localScores;
 		
 		// Load Mlb Player Stats
 		float [] mlbScores  = {mlbPlayer.getHitting_games_play(), mlbPlayer.getHitting_ab(), mlbPlayer.getHitting_runs_total(), mlbPlayer.getHitting_onbase_h(), 
 				mlbPlayer.getHitting_onbase_d(), mlbPlayer.getHitting_onbase_t(), mlbPlayer.getHitting_onbase_hr(), mlbPlayer.getHitting_rbi(),
-				mlbPlayer.getHitting_outs_ktotal(), mlbPlayer.getHitting_steal_stolen(), mlbPlayer.getBattingAverage()};
+				mlbPlayer.getHitting_outs_ktotal(), mlbPlayer.getHitting_steal_stolen()};
 		mlbScore = mlbScores;
+		mlbBAverage = mlbPlayer.getBattingAverage();
+		mlbSlug = mlbPlayer.getSlugging();
 		System.out.println("Not implemented yet");
 	}
 
