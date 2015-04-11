1d8a04cd55a22bab0a29eef1ba132d240f31b0cf
Colton <carose3@asu.edu>
2015-04-11 10:08:14 -0700

    Nearly finished ComparePlayers array loading method

--- src/database/ComparePlayers.java
+++ src/database/ComparePlayers.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 
 public class ComparePlayers {
 	
-	private static float[] localScores;
-	private static float[] mlbScores;
+	private static float[] localScore;
+	private static float[] mlbScore;
 	private static float[] differenceNums;
 	
 	public static ArrayList<MlbPlayer> compareToPlayerList(LocalPlayer lp, ArrayList<MlbPlayer> mlbList) {
@@ -14,9 +14,9 @@ public class ComparePlayers {
 	
 	public static float compareToPlayer(LocalPlayer lb, MlbPlayer mlbPlayer) {
 		loadScores(lb, mlbPlayer);
-		int startScore = 1000;
+		float startScore = 100.00f;
 		for(int i = 0; i < differenceNums.length; i++) {
-			startScore -= returnDifference(localScores[i], mlbScores[i]);
+			startScore -= returnDifference(localScore[i], mlbScore[i]);
 		}
 		return 50.0f;
 	}
@@ -33,9 +33,13 @@ public class ComparePlayers {
 		float[] localScores = {lbStats.getHitting_games_play(), lbStats.getHitting_ab(), lbStats.getHitting_runs_total(), lbStats.getHitting_onbase_h(), 
 				lbStats.getHitting_onbase_d(), lbStats.getHitting_onbase_t(), lbStats.getHitting_onbase_hr(), lbStats.getHitting_rbi(),
 				lbStats.getHitting_outs_ktotal(), lbStats.getHitting_steal_stolen()};
+		localScore = localScores;
 		
 		// Load Mlb Player Stats
-		float [] mlbScores = {};
+		float [] mlbScores  = {mlbPlayer.getHitting_games_play(), mlbPlayer.getHitting_ab(), mlbPlayer.getHitting_runs_total(), mlbPlayer.getHitting_onbase_h(), 
+				mlbPlayer.getHitting_onbase_d(), mlbPlayer.getHitting_onbase_t(), mlbPlayer.getHitting_onbase_hr(), mlbPlayer.getHitting_rbi(),
+				mlbPlayer.getHitting_outs_ktotal(), mlbPlayer.getHitting_steal_stolen()};
+		mlbScore = mlbScores;
 		System.out.println("Not implemented yet");
 	}
 
