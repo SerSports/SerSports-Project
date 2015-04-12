311d0029e9ee5a70c1affa44282da36addfdb51a
gtikhono <gtikhono@asu.edu>
2015-04-12 08:44:08 -0700

    sign in page minor change

--- src/gui/UserLoginGUI.java
+++ src/gui/UserLoginGUI.java
@@ -33,14 +33,13 @@ import database.User;
  * Description: GUI for main navigation of site
  */
 public class UserLoginGUI extends JPanel {
+	protected JTextField txtUserName = new JTextField();
 	protected JPasswordField pwdPassword = new JPasswordField();
 	protected JButton btnSubmit = new JButton("SIGN IN");
 	protected JButton btnCreateAccount_1 = new JButton("CREATE ACCOUNT");
 	protected JButton btnCreateAccount;
-	protected JTextField txtUserName = new JTextField();
 	protected Image img = new ImageIcon(this.getClass().getResource(
 			"/images/Backgroundimage.png")).getImage();
-	protected JLabel line = new JLabel("");
 	protected Image lineimg = new ImageIcon(this.getClass().getResource(
 			"/images/LineSignInPage.png")).getImage();
 	protected JLabel lblbackgroundImage = new JLabel("");
@@ -51,35 +50,36 @@ public class UserLoginGUI extends JPanel {
 
 	public UserLoginGUI() {
 		setLayout(null);
-		setLocation(dim.width / 2 - this.getSize().width / 2,dim.height / 2 - this.getSize().height / 2);
-		txtUserName.setBackground(new Color(244, 229, 192));
+		setPreferredSize(new Dimension(1000, 650));
+		txtUserName.setFont(new Font("Malayalam Sangam MN", Font.PLAIN, 14));
+
+		
+		txtUserName.setBackground(new Color(47, 52, 64));
+		txtUserName.setForeground(new Color(244, 229, 192));
 		txtUserName.setBounds(432, 199, 135, 28);
 		txtUserName.setText("ser_sports");
 		add(txtUserName);
+		pwdPassword.setFont(new Font("Malayalam Sangam MN", Font.PLAIN, 14));
 
-		pwdPassword.setBackground(new Color(244, 229, 192));
+		pwdPassword.setBackground(new Color(47, 52, 64));
+		pwdPassword.setForeground(new Color(244, 229, 192));
 		pwdPassword.setBounds(432, 246, 135, 28);
 		pwdPassword.setText("admin");
 		add(pwdPassword);
 
-		btnSubmit.setBounds(456, 319, 88, 29);
+		btnSubmit.setBounds(456, 301, 88, 29);
 		add(btnSubmit);
 		btnSubmit.setBorder(BorderFactory.createEmptyBorder());
 		btnSubmit.setContentAreaFilled(false);
 		btnSubmit.setForeground(new Color(244, 229, 192));
 		btnSubmit.setFont(new Font("jaf lapture", Font.PLAIN, 18));
 
-		btnCreateAccount_1.setBounds(417, 360, 166, 28);
+		btnCreateAccount_1.setBounds(417, 342, 166, 28);
 		add(btnCreateAccount_1);
 		btnCreateAccount_1.setBorder(BorderFactory.createEmptyBorder());
 		btnCreateAccount_1.setContentAreaFilled(false);
 		btnCreateAccount_1.setForeground(new Color(244, 229, 192));
 		btnCreateAccount_1.setFont(new Font("jaf lapture", Font.PLAIN, 18));
-
-		// create and index the Panels
-		line.setBounds(350, 253, 300, 107);
-		add(line);
-		line.setIcon(new ImageIcon(lineimg));
 		
 		lblbackgroundImage.setBounds(-908, -627, 3456, 1446);
 		add(lblbackgroundImage);
