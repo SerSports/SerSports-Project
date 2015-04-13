57d65ba053d941468d71c8744c585c5e835ff723
gtikhono <gtikhono@asu.edu>
2015-04-13 13:49:14 -0700

    adding code documentation

--- src/gui/UserLoginGUI.java
+++ src/gui/UserLoginGUI.java
@@ -38,42 +38,40 @@ public class UserLoginGUI extends JPanel {
 	protected JButton btnSubmit = new JButton("SIGN IN");
 	protected JButton btnCreateAccount_1 = new JButton("CREATE ACCOUNT");
 	protected JButton btnCreateAccount;
-	protected Image img = new ImageIcon(this.getClass().getResource(
-			"/images/Backgroundimage.png")).getImage();
-	protected Image lineimg = new ImageIcon(this.getClass().getResource(
-			"/images/LineSignInPage.png")).getImage();
+	protected Image img = new ImageIcon(this.getClass().getResource("/images/Backgroundimage.png")).getImage();
 	protected JLabel lblbackgroundImage = new JLabel("");
 	protected Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 	MainGUI main = null;
 
-	// private static MainGUI singleton = null;
-
 	public UserLoginGUI() {
 		setLayout(null);
 		setPreferredSize(new Dimension(1000, 650));
-		txtUserName.setFont(new Font("Malayalam Sangam MN", Font.PLAIN, 14));
-
 		
+		//username text field
+		txtUserName.setFont(new Font("Malayalam Sangam MN", Font.PLAIN, 14));
 		txtUserName.setBackground(new Color(47, 52, 64));
 		txtUserName.setForeground(new Color(244, 229, 192));
 		txtUserName.setBounds(432, 199, 135, 28);
 		txtUserName.setText("ser_sports");
 		add(txtUserName);
+		
+		//password text field
 		pwdPassword.setFont(new Font("Malayalam Sangam MN", Font.PLAIN, 14));
-
 		pwdPassword.setBackground(new Color(47, 52, 64));
 		pwdPassword.setForeground(new Color(244, 229, 192));
 		pwdPassword.setBounds(432, 246, 135, 28);
 		pwdPassword.setText("admin");
 		add(pwdPassword);
 
+		//sign in button
 		btnSubmit.setBounds(456, 301, 88, 29);
 		add(btnSubmit);
 		btnSubmit.setBorder(BorderFactory.createEmptyBorder());
 		btnSubmit.setContentAreaFilled(false);
 		btnSubmit.setForeground(new Color(244, 229, 192));
 		btnSubmit.setFont(new Font("jaf lapture", Font.PLAIN, 18));
-
+		
+		//create account button
 		btnCreateAccount_1.setBounds(417, 342, 166, 28);
 		add(btnCreateAccount_1);
 		btnCreateAccount_1.setBorder(BorderFactory.createEmptyBorder());
@@ -81,18 +79,19 @@ public class UserLoginGUI extends JPanel {
 		btnCreateAccount_1.setForeground(new Color(244, 229, 192));
 		btnCreateAccount_1.setFont(new Font("jaf lapture", Font.PLAIN, 18));
 		
+		//background image
 		lblbackgroundImage.setBounds(-908, -627, 3456, 1446);
 		add(lblbackgroundImage);
 		lblbackgroundImage.setIcon(new ImageIcon(img));
 
-
+		//create account button action Listener
 		btnCreateAccount_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				main.showCreateAccount();
-
 			}
 		});
 		
+		//sign in action button Listener
 		btnSubmit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				User user = User.authenticateUser(txtUserName.getText(), new String(pwdPassword.getPassword()));
@@ -101,16 +100,9 @@ public class UserLoginGUI extends JPanel {
 				} else {
 				JOptionPane.showMessageDialog(null, "Invalid Username / Password!", "InfoBox: SER SPORTS", JOptionPane.INFORMATION_MESSAGE);
 				}
-				
-
 			}
-			
-		
 		});
 
 	}
-	/*
-	 * public void loadUserInfoIntoControls() {
-	 *     main.loadUserInfoIntoControls();     }
-	 */
+
 }
