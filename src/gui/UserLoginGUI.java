d5957415505906a58b390062c0462809e8f78a89
gtikhono <gtikhono@asu.edu>
2015-04-13 14:08:08 -0700

    moving stuff around

--- src/gui/UserLoginGUI.java
+++ src/gui/UserLoginGUI.java
@@ -15,7 +15,6 @@ import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
