29fed3ec9b60ad62454b9b9567bfeb62e3b031f2
gtikhono <gtikhono@asu.edu>
2015-04-14 11:32:27 -0700

    Cleaning, FindBugs XML and deleting unused classes

--- src/gui/ContactUs.java
+++ src/gui/ContactUs.java
@@ -13,11 +13,7 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.JButton;
 import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.Font;
 
 
@@ -27,6 +23,11 @@ Class: ContactUs
 Description: GUI for the Contact Us Page
 */
 public class ContactUs extends JPanel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3855995346503974075L;
+
 	/**
 	  Method: Constructor
 	  Inputs: None
@@ -39,24 +40,25 @@ public class ContactUs extends JPanel {
 		setBackground(new Color(47, 52, 64));
 		setLayout(null);
 		
+		//Logo
 		JLabel logo = new JLabel("");
 		logo.setBounds(6, 0, 152, 55);
 		add(logo);
 		Image img = new ImageIcon(this.getClass().getResource("/images/LogoBottom.png")).getImage();
 		logo.setIcon(new ImageIcon(img));
 		
+		//Email Information
 		JLabel lblEmailUs = new JLabel("Email Us: ser.sports.asu@gmail.com");
 		lblEmailUs.setFont(new Font("Bangla MN", Font.PLAIN, 18));
 		lblEmailUs.setForeground(new Color(244, 229, 192));
 		lblEmailUs.setBounds(81, 167, 369, 29);
 		add(lblEmailUs);
 		
+		//Phone Information
 		JLabel lblCallUs = new JLabel("Call Us: (555) 555-5555");
 		lblCallUs.setFont(new Font("Bangla MN", Font.PLAIN, 18));
 		lblCallUs.setForeground(new Color(244, 229, 192));
 		lblCallUs.setBounds(81, 203, 251, 29);
 		add(lblCallUs);
-			
-
 	}
 }
