bfcd2de795c478d9f6f171e4c97005042ebb6614
aamannon <aamannon@asu.edu>
2015-04-18 13:19:34 -0700

    testing install & image api
    created executable jar and skeleton of image api class

---
+++ src/database/SportsPicsApi.java
@@ -0,0 +1,49 @@
+/*
+File: SportsStatsApi.java
+Author:	XML Code adapted from 
+		http://www.javacodegeeks.com/2013/05/parsing-xml-using-dom-sax-and-stax-parser-in-java.html
+Date:	
+
+Description: API for pulling the MLB stats from Sports Data LLC
+
+ */
+package database;
+
+/*
+ 
+ API for http://www.sportsdatallc.com/
+ */
+import javax.xml.parsers.*;
+
+import java.awt.image.BufferedImage;
+import java.net.*;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
+import java.util.*;
+
+public class SportsPicsApi {
+	private static final String apiKey = "enwf769pmzvpjr57jurvexqz";
+	private static final String apiUrlPrefix = "https://api.sportsdatallc.org/mlb-images-t4/usat/players/headshot/";
+	private static final String apiUrlSuffix = ".jpg?api_key=";
+	
+	//http(s)://api.sportsdatallc.org/[sport]-images-[access_level][version]/
+	//[provider]/players/[image_type]/[asset_id]/[filename].[format]?api_key=[your_api_key]
+		
+	public static BufferedImage getImageForMlbPlayer(String assetId) {
+		BufferedImage pic = null;
+		
+		
+		return pic;
+	}
+	public static void loadNewestStatsIntoDatabase() {
+
+	}
+
+	private static String getApiUrlString(String assetId) {
+		return apiUrlPrefix + assetId + apiUrlSuffix + apiKey;
+	}
+}
