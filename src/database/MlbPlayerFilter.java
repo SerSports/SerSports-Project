1ddb5cad13d8507fef60ea018e82895cce6bdd5e
Nick Krogstad <nickkrogstad@gmail.com>
2015-04-22 04:32:07 -0700

    Documentation: MlbPlayerFilter

--- src/database/MlbPlayerFilter.java
+++ src/database/MlbPlayerFilter.java
@@ -1,49 +1,94 @@
 package database;
 
 /**
-Class:	MlbPlayerFilter
-
-Description: This class is used to create the Filter Criteria that the MlbPlayer Class uses.
-*/
-public class MlbPlayerFilter {
-
-	// Values to Filter By
+ * Creates Filter Criteria to better manage data in the MlbPlayer Class
+ * 
+ * @author SerSports
+ */
+public class MlbPlayerFilter
+{
+	
+	/*
+	 * Filter Values
+	 */
 	private String idValue = null;
 	private String firstNameValue = null;
 	private String lastNameValue = null;
 	private String teamNameValue = null;
-
-	// Setters
-	public void setIdValue(String idValue) {
-		if (idValue != null && idValue.length() > 0) {
+	
+	/**
+	 * Ensures the Id has a value and is not empty
+	 * 
+	 * @param idValue
+	 *            Identifier assigned to all players
+	 */
+	public void setIdValue(String idValue)
+	{
+		if (idValue != null && idValue.length() > 0)
+		{
 			this.idValue = idValue;
 		}
 	}
-	public void setFirstNameValue(String firstNameValue) {
-		if (firstNameValue != null && firstNameValue.length() > 0 && !firstNameValue.equals("First Name")) {
+	
+	/**
+	 * Ensures the first name has a value and is not empty
+	 * 
+	 * @param firstNameValue
+	 *            Player's first name
+	 */
+	public void setFirstNameValue(String firstNameValue)
+	{
+		if (firstNameValue != null && firstNameValue.length() > 0
+				&& !firstNameValue.equals("First Name"))
+		{
 			this.firstNameValue = firstNameValue;
 		}
 	}
-	public void setLastNameValue(String lastNameValue) {
-		if (lastNameValue != null && lastNameValue.length() > 0 && !lastNameValue.equals("Last Name")) {
+	
+	/**
+	 * Ensures the last name has a value and is not empty
+	 * 
+	 * @param lastNameValue
+	 *            Player's last name
+	 */
+	public void setLastNameValue(String lastNameValue)
+	{
+		if (lastNameValue != null && lastNameValue.length() > 0
+				&& !lastNameValue.equals("Last Name"))
+		{
 			this.lastNameValue = lastNameValue;
 		}
 	}
-	public void setTeamNameValue(String teamNameValue) {
-		if (teamNameValue != null && teamNameValue.length() > 0 && !teamNameValue.equals("Team")) {
+	
+	/**
+	 * Ensures the team name has a value and is not empty
+	 * 
+	 * @param teamNameValue
+	 *            Name of the Player's team
+	 */
+	public void setTeamNameValue(String teamNameValue)
+	{
+		if (teamNameValue != null && teamNameValue.length() > 0
+				&& !teamNameValue.equals("Team"))
+		{
 			this.teamNameValue = teamNameValue;
 		}
 	}
-
-	// Methods
-	public String getWhereClause() {
+	
+	/**
+	 * Checks to see if any values were passed in and adds the necessary fields to a
+	 * string builder
+	 * 
+	 * @return String built from the fields that passed through
+	 */
+	public String getWhereClause()
+	{
 		StringBuilder whereClause = new StringBuilder();
 		String result = new String("");
 		
-		// See if anything was passed in
-		if (idValue != null || firstNameValue != null || lastNameValue != null || teamNameValue != null) {
-					
-			// Add the fields we need
+		if (idValue != null || firstNameValue != null || lastNameValue != null
+				|| teamNameValue != null)
+		{
 			whereClause = addCriteriaToStringBuilder(whereClause, "mlbPlayerId", idValue);
 			whereClause = addCriteriaToStringBuilder(whereClause, "firstName", firstNameValue);
 			whereClause = addCriteriaToStringBuilder(whereClause, "lastName", lastNameValue);
@@ -51,21 +96,33 @@ public class MlbPlayerFilter {
 			
 			result = "WHERE " + whereClause.toString();
 		}
+		
 		return result;
 	}
 	
-	private StringBuilder addCriteriaToStringBuilder(StringBuilder builder, String fieldName, String value) {
-	
-		if (value != null) {
-			
-			// If there is already criteria, add an "AND" 
-			if (builder.length() > 0) {
+	/**
+	 * Concatenates criteria with an "AND" if there are multiple filters induced
+	 * 
+	 * @param builder
+	 *            StringBuilder being appended to
+	 * @param fieldName
+	 *            Name of the filter being applied
+	 * @param value
+	 *            Value that qualifies for the filter
+	 * @return
+	 */
+	private StringBuilder addCriteriaToStringBuilder(StringBuilder builder,
+			String fieldName, String value)
+	{
+		
+		if (value != null)
+		{
+			if (builder.length() > 0)
+			{
 				builder.append(" AND ");
 			}
 			
-			// Add the criteria
 			builder.append(fieldName + " = \"" + value + "\"");
-			
 		}
 		
 		return builder;
