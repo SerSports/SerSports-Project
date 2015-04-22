f311705cef5b91f02b16843468de42ccc4a4eb1a
Nick Krogstad <nickkrogstad@gmail.com>
2015-04-22 05:15:15 -0700

    Documentation: SportsPicsApi

--- src/database/SportsPicsApi.java
+++ src/database/SportsPicsApi.java
@@ -1,12 +1,3 @@
-/*
-File: SportsStatsApi.java
-Author:	XML Code adapted from 
-		http://www.javacodegeeks.com/2013/05/parsing-xml-using-dom-sax-and-stax-parser-in-java.html
-Date:	
-
-Description: API for pulling the MLB stats from Sports Data LLC
-
- */
 package database;
 
 import java.awt.Image;
@@ -27,49 +18,82 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-/*
- 
- API for http://www.sportsdatallc.com/
+/**
+ * API used for pulling the MLB statistics from the database Sports Data LLC
+ * 
+ * @author XML Code adapted from
+ *         http://www.javacodegeeks.com/2013/05/parsing-xml-using-dom
+ *         -sax-and-stax-parser-in-java.html
  */
-
-public class SportsPicsApi {
+public class SportsPicsApi
+{
 	private static final String apiImageKey = "udwx6be6kaszg2y46z6aduj2";
 	private static final String apiImageUrlPrefix = "https://api.sportsdatallc.org/mlb-images-t2/usat/players/headshots/";
 	private static final String apiImageUrlSuffix = "/250.jpg?api_key=";
-		
-	public static Image getImageForMlbPlayer(MlbPlayer player) {
+	
+	/**
+	 * Gets an image that matches the Asset Id assigned to each player object
+	 * 
+	 * @param player
+	 *            Player whose image is being obtained
+	 * @return Image of the player whose Asset Id is matched
+	 */
+	public static Image getImageForMlbPlayer(MlbPlayer player)
+	{
 		Image pic = null;
 		
-		try {
+		try
+		{
 			NodeList nodeList = getManifestNodeList();
 			String assetId = getAssetIdFromManifestForMlbPlayerId(nodeList, player);
 			pic = getImageFromAssetId(assetId);
 		}
-		catch (Exception e) {
-			try {
-				URL url = new URL("http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/4937.png&w=350&h=254");
+		catch (Exception e)
+		{
+			try
+			{
+				URL url = new URL(
+						"http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/4937.png&w=350&h=254");
 				pic = ImageIO.read(url);
-			} catch (MalformedURLException me) {
+			}
+			catch (MalformedURLException me)
+			{
 				me.printStackTrace();
- 			} catch (IOException ioe) {
- 				ioe.printStackTrace();
- 			}
+			}
+			catch (IOException ioe)
+			{
+				ioe.printStackTrace();
+			}
 		}
 		
 		return pic;
 	}
-
-	private static String getAssetIdFromManifestForMlbPlayerId(
-			NodeList nodeList, MlbPlayer player) {
+	
+	/**
+	 * Uses Manifest Records to get an Asset Id for any given MLB Player
+	 * 
+	 * @param nodeList
+	 *            List of nodes consisting of MLB Player's Id's
+	 * @param player
+	 *            MLB Player object
+	 * @return Identifier for an MLB Player
+	 */
+	private static String getAssetIdFromManifestForMlbPlayerId(NodeList nodeList,
+			MlbPlayer player)
+	{
 		String assetID = null;
 		
-		if (nodeList != null) {
-			for (int i = 0; i < nodeList.getLength() && assetID == null; i++) {
+		if (nodeList != null)
+		{
+			for (int i = 0; i < nodeList.getLength() && assetID == null; i++)
+			{
 				Node node = nodeList.item(i);
-				if (node instanceof Element) {
+				if (node instanceof Element)
+				{
 					ManifestRecord record = new ManifestRecord(node);
 					
-					if (record.getPlayerId().equals(player.getId())) {
+					if (record.getPlayerId().equals(player.getId()))
+					{
 						assetID = record.getAssetId();
 					}
 				}
@@ -79,23 +103,46 @@ public class SportsPicsApi {
 		return assetID;
 	}
 	
-	// Working Example: (WARNING our license only allows 25 calls a month, so don't follow this link unless you NEED to)
-	// https://api.sportsdatallc.org/mlb-images-t2/usat/players/headshots/c6070f31-749f-4e99-8f35-f2a8bce3be4a/250.jpg?api_key=udwx6be6kaszg2y46z6aduj2
-	private static Image getImageFromAssetId(String assetId) throws MalformedURLException, IOException {
-		
+	/**
+	 * WARNING: Our license only allows 25 calls a month, so don't follow this link unless
+	 * you NEED to, this is just a working example.
+	 * 
+	 * https://api.sportsdatallc.org/mlb-images-t2/usat/players
+	 * /headshots/c6070f31-749f-4e99
+	 * -8f35-f2a8bce3be4a/250.jpg?api_key=udwx6be6kaszg2y46z6aduj2
+	 * 
+	 * Sets the URL String to an arbitrary value in order to prevent it from using one of
+	 * the API's calls for the month
+	 * 
+	 * @param assetId
+	 *            Identifier assigned to every player
+	 * @return Image that was generated
+	 * @throws MalformedURLException
+	 * @throws IOException
+	 */
+	private static Image getImageFromAssetId(String assetId)
+			throws MalformedURLException, IOException
+	{
 		String urlString = getApiUrlString(assetId);
-		urlString = new String("12312");				// Only here to mess things up.  We don't want this code to work otherwise it will use one of our API's calls for this month.
+		urlString = new String("12312"); // Arbitrary Value
 		URL url = new URL(urlString);
 		Image pic = ImageIO.read(url);
 		
 		return pic;
 	}
-
-	private static NodeList getManifestNodeList() throws UnableToLoadManifestException {
+	
+	/**
+	 * Gets the elements of the Manifest Records and returns them as a list of nodes
+	 * 
+	 * @return NodeList consisting of the elements in the Manifest Records
+	 * @throws UnableToLoadManifestException
+	 */
+	private static NodeList getManifestNodeList() throws UnableToLoadManifestException
+	{
 		NodeList list = null;
 		
-		try {
-			
+		try
+		{
 			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder builder = factory.newDocumentBuilder();
 			
@@ -103,32 +150,39 @@ public class SportsPicsApi {
 			Document document = builder.parse(manifest);
 			
 			list = document.getDocumentElement().getChildNodes();
-			
-		} catch (ParserConfigurationException e) {
-			// TODO Auto-generated catch block
+		}
+		catch (ParserConfigurationException e)
+		{
 			e.printStackTrace();
 			throw new UnableToLoadManifestException();
-		} catch (FileNotFoundException e) {
-			System.out.println("Error in getManifestNodeList():  Can't find manifest file!");
+		}
+		catch (FileNotFoundException e)
+		{
+			System.out
+					.println("Error in getManifestNodeList():  Can't find manifest file!");
 			throw new UnableToLoadManifestException();
-		} catch (SAXException e) {
-			// TODO Auto-generated catch block
+		}
+		catch (SAXException e)
+		{
 			e.printStackTrace();
 			throw new UnableToLoadManifestException();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+		}
+		catch (IOException e)
+		{
 			e.printStackTrace();
 			throw new UnableToLoadManifestException();
 		}
 		
 		return list;
 	}
-
-	private static String getApiUrlString(String assetId) {
+	
+	private static String getApiUrlString(String assetId)
+	{
 		return apiImageUrlPrefix + assetId + apiImageUrlSuffix + apiImageKey;
 	}
 	
-	private static class UnableToLoadManifestException extends Exception {
-		
+	private static class UnableToLoadManifestException extends Exception
+	{
+		// TODO: Add functionality
 	}
 }
