cf5fe4da7fb7bcf41d65be07abc4bae13b9197b7
Nick Krogstad <nickkrogstad@gmail.com>
2015-04-29 13:32:39 -0700

    Documentation: ModifiedFocusAdapter

--- src/gui/ModifiedFocusAdapter.java
+++ src/gui/ModifiedFocusAdapter.java
@@ -1,30 +1,51 @@
 package gui;
+
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
 import javax.swing.JTextField;
 
-public class ModifiedFocusAdapter extends FocusAdapter {
+/**
+ * Handles all user entry fields by creating the actual text field where they enter
+ * information and displaying a caption that tells the user what information goes in the
+ * box. Reverts back to the default caption if the box is clicked away from or manipulated
+ * 
+ * @author SerSports
+ */
+public class ModifiedFocusAdapter extends FocusAdapter
+{
 	
-		//Members
-	    protected JTextField txtField = new JTextField();
-	    protected String caption;
-	    
-	    //Constructor
-	    public ModifiedFocusAdapter (JTextField txt, String caption) {
-	    	this.txtField = txt;
-	    	this.caption = caption;	    	
-	    }
-
-	    //Methods to Override text when focus is lost or gained
-	    @Override
-	    public void focusGained(FocusEvent e){
-	        if (txtField.getText().equals(caption)){
-	        txtField.setText(""); }
-	    }
-	    @Override
-	    public void focusLost(FocusEvent e){
-	        if (txtField.getText().equals("")){
-	        	txtField.setText(caption);}
-	    }
+	protected JTextField txtField = new JTextField();
+	protected String caption;
+	
+	/**
+	 * Sets the text field and caption
+	 * 
+	 * @param txt
+	 *            Text field where the user enters whatever is required
+	 * @param caption
+	 *            Caption letting the user know what to enter
+	 */
+	public ModifiedFocusAdapter(JTextField txt, String caption)
+	{
+		this.txtField = txt;
+		this.caption = caption;
 	}
-
+	
+	@Override
+	public void focusGained(FocusEvent e)
+	{
+		if (txtField.getText().equals(caption))
+		{
+			txtField.setText("");
+		}
+	}
+	
+	@Override
+	public void focusLost(FocusEvent e)
+	{
+		if (txtField.getText().equals(""))
+		{
+			txtField.setText(caption);
+		}
+	}
+}
