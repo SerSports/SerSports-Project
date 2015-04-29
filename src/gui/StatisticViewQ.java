9fcd1cf48f7d28d32ef80e1077882e3b7b494ca1
Nick Krogstad <nickkrogstad@gmail.com>
2015-04-29 13:01:13 -0700

    Documentation: StatisticViewQ

--- src/gui/StatisticViewQ.java
+++ src/gui/StatisticViewQ.java
@@ -7,11 +7,17 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 
+/**
+ * Displays a panel on the FAQ page containing the answer to the question
+ * "Will other people be able to view my statistics?"
+ * 
+ * @author SerSports
+ */
 public class StatisticViewQ extends JPanel
 {
 	
 	/**
-	 * Create the panel.
+	 * Creates the panel containing the answer to the question
 	 */
 	public StatisticViewQ()
 	{
@@ -19,7 +25,8 @@ public class StatisticViewQ extends JPanel
 		setPreferredSize(new Dimension(345, 424));
 		setLayout(null);
 		
-		JLabel StatisticViewA = new JLabel("<html>Once you submit your statistics on the website, they will be available to view by other users on the “Browse Local Players” page. If you would like to avoid this situation, simply delete your statistics.</html>");
+		JLabel StatisticViewA = new JLabel(
+				"<html>Once you submit your statistics on the website, they will be available to view by other users on the “Browse Local Players” page. If you would like to avoid this situation, simply delete your statistics.</html>");
 		StatisticViewA.setPreferredSize(new Dimension(345, 424));
 		StatisticViewA.setBounds(97, 116, 151, 192);
 		StatisticViewA.setForeground(new Color(244, 229, 192));
