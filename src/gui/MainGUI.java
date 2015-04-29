473e245e0023a92402908022ae928ee39f71432b
Nick Krogstad <nickkrogstad@gmail.com>
2015-04-29 13:14:11 -0700

    Documentation: MainGUI

--- src/gui/MainGUI.java
+++ src/gui/MainGUI.java
@@ -1,11 +1,3 @@
-/*
-File: MainGUI.java
-Author:	
-Date:	
-
-Description: Main GUI (the brain, the tabs)
-
- */
 package gui;
 
 import java.awt.CardLayout;
@@ -15,15 +7,14 @@ import java.awt.EventQueue;
 import java.awt.Toolkit;
 import javax.swing.*;
 
-
-
 /**
- * Class: MainGUI
+ * GUI used as the backbone of the application that provides all transition functionality
+ * between pages
  * 
- * Description: GUI for main navigation of site
+ * @author SerSports
  */
-public class MainGUI {
-	
+public class MainGUI
+{
 	private static MainGUI singleton = null;
 	JFrame mainFrame = new JFrame("SERSports");
 	protected final static ApplicationGUI panelApplication = new ApplicationGUI();
@@ -33,42 +24,54 @@ public class MainGUI {
 	static CardLayout c1 = new CardLayout();
 	JScrollPane scrollPane = new JScrollPane();
 	Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-
-	public static void showLoginGUI() {
+	
+	public static void showLoginGUI()
+	{
 		c1.show(panelContainer, "1");
 	}
-
-	public static void showApplicationGUI() {
+	
+	public static void showApplicationGUI()
+	{
 		panelApplication.loadUserInfoIntoControls();
 		c1.show(panelContainer, "2");
-		
 	}
-
-	public static void showCreateAccount() {
+	
+	public static void showCreateAccount()
+	{
 		c1.show(panelContainer, "3");
 	}
-
-	public static void setApplicationToClose() {
-		if (singleton != null) {
+	
+	public static void setApplicationToClose()
+	{
+		if (singleton != null)
+		{
 			singleton.mainFrame.dispose();
 			singleton.mainFrame.repaint();
 			singleton = new MainGUI();
-			
 		}
 	}
 	
-	public static void setHourGlass(Boolean hourGlassOn) {
-		if (singleton != null) {
-			if (hourGlassOn) {
-				singleton.mainFrame.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
-			} else {
+	public static void setHourGlass(Boolean hourGlassOn)
+	{
+		if (singleton != null)
+		{
+			if (hourGlassOn)
+			{
+				singleton.mainFrame.setCursor(Cursor
+						.getPredefinedCursor(Cursor.WAIT_CURSOR));
+			}
+			else
+			{
 				singleton.mainFrame.setCursor(Cursor.getDefaultCursor());
 			}
 		}
 	}
-
-	public MainGUI() {
-		
+	
+	/**
+	 * Displays all components of the upper tabs together
+	 */
+	public MainGUI()
+	{
 		mainFrame.getContentPane().add(scrollPane);
 		scrollPane.setBorder(BorderFactory.createEmptyBorder());
 		scrollPane.setViewportView(panelContainer);
@@ -78,36 +81,34 @@ public class MainGUI {
 		panelContainer.add(login, "1");
 		panelContainer.add(panelApplication, "2");
 		panelContainer.add(createAccountGUI, "3");
-
-
+		
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainFrame.pack();
 		mainFrame.setVisible(true);
-		mainFrame.setLocation(dim.width / 2 - mainFrame.getSize().width / 2,
-				dim.height / 2 - mainFrame.getSize().height / 2);
-
+		mainFrame.setLocation(dim.width / 2 - mainFrame.getSize().width / 2, dim.height
+				/ 2 - mainFrame.getSize().height / 2);
 	}
-
-	/**
-	 * Method: main Inputs: String[] args Returns: NA
-	 * 
-	 * Description:
-	 */
-
-	public static void main(String[] args) {
+	
+	public static void main(String[] args)
+	{
 		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
+			public void run()
+			{
+				try
+				{
 					
 					singleton = new MainGUI();
-				} catch (Exception e) {
+				}
+				catch (Exception e)
+				{
 					e.printStackTrace();
 				}
 			}
 		});
 	}
-
-	public void loadUserInfoIntoControls() {
+	
+	public void loadUserInfoIntoControls()
+	{
 		panelApplication.loadUserInfoIntoControls();
 	}
 }
