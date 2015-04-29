8e7bbfa2187d04cfa27865a59684b834e27f4f6d
Nick Krogstad <nickkrogstad@gmail.com>
2015-04-29 12:15:01 -0700

    Documentation: AgeRequirementQ

--- src/gui/AgeRequirementQ.java
+++ src/gui/AgeRequirementQ.java
@@ -6,16 +6,18 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 
+/**
+ * Displays a panel on the FAQ page containing the answer to the question
+ * "Is there an age requirement to sign up?"
+ * 
+ * @author SerSports
+ */
 public class AgeRequirementQ extends JPanel
 {
+	private static final long serialVersionUID = 3282099479400601681L;
 	
 	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 3282099479400601681L;
-
-	/**
-	 * Create the panel.
+	 * Creates the panel containing the answer to the question
 	 */
 	public AgeRequirementQ()
 	{
@@ -24,7 +26,8 @@ public class AgeRequirementQ extends JPanel
 		setToolTipText("");
 		setLayout(null);
 		
-		JLabel AgeRequirementA = new JLabel("<html>The age requirement for creating an account is 13 years of age or older. A parent or guardian's approval is required for children who do not meet the age requirement.</html>");
+		JLabel AgeRequirementA = new JLabel(
+				"<html>The age requirement for creating an account is 13 years of age or older. A parent or guardian's approval is required for children who do not meet the age requirement.</html>");
 		AgeRequirementA.setPreferredSize(new Dimension(345, 424));
 		AgeRequirementA.setBounds(97, 116, 151, 192);
 		AgeRequirementA.setFont(new Font("Myanmar Sangam MN", Font.PLAIN, 14));
