3b96c6f578e394df84956ecc6c6e9b1d0adfcc8d
Nick Krogstad <nickkrogstad@gmail.com>
2015-04-29 12:59:57 -0700

    Documentation: SimilarityQ

--- src/gui/SimilarityQ.java
+++ src/gui/SimilarityQ.java
@@ -8,18 +8,25 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 
+/**
+ * Displays a panel on the FAQ page containing the answer to the question
+ * "How is the similarity between players determined?"
+ * 
+ * @author SerSports
+ */
 public class SimilarityQ extends JPanel
 {
 	
 	/**
-	 * Create the panel.
+	 * Creates the panel containing the answer to the question
 	 */
 	public SimilarityQ()
 	{
 		setLayout(null);
 		setBackground(new Color(47, 52, 64));
 		setPreferredSize(new Dimension(345, 424));
-		JLabel SimilarityA = new JLabel("<html>All statistics <br/>that are entered by the user are taken in to account when determining the similarity between said user and an MLB player. The closer the entered statistics are to an MLB player, the higher their similarity percentage is.</html>");
+		JLabel SimilarityA = new JLabel(
+				"<html>All statistics <br/>that are entered by the user are taken in to account when determining the similarity between said user and an MLB player. The closer the entered statistics are to an MLB player, the higher their similarity percentage is.</html>");
 		SimilarityA.setHorizontalAlignment(SwingConstants.CENTER);
 		SimilarityA.setBounds(97, 98, 151, 228);
 		SimilarityA.setFont(new Font("Myanmar Sangam MN", Font.PLAIN, 14));
